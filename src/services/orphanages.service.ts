474679ac12611ff2a215c52a1898775a718ef1bc
Alison Marques <webdev@pop-os.localdomain>
2020-10-21 23:39:19 -0300

    refatorando schema

--- src/services/orphanages.service.ts
+++ src/services/orphanages.service.ts
@@ -1,48 +1,36 @@
 import * as Yup from 'yup';
-import httpStatus from 'http-status'
+import httpStatus from 'http-status';
 import { getRepository } from 'typeorm';
 
+import schemaOrphanage from '../schemas/schemaOrphanage';
 import Orphanage from '../models/Orphanage';
 
 class OrphanagesService {
-
   index = async () => {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.find({ relations: ['images'] })
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
 
     return {
       status: httpStatus.OK,
-      result: orphanages
+      result: orphanages,
     };
-  }
+  };
 
   show = async (id: any) => {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanage = await orphanagesRepository.findOneOrFail(id, { relations: ['images'] });
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
     return {
       status: httpStatus.OK,
-      result: orphanage
+      result: orphanage,
     };
-  }
+  };
 
   create = async (data: any) => {
-    // Criando a validação dos dados
-    const schema = Yup.object().shape({
-      name: Yup.string().required('Nome obrigatório'),
-      latitude: Yup.number().required(),
-      longitude: Yup.number().required(),
-      about: Yup.string().required().max(300),
-      instructions: Yup.string().required(),
-      opening_hours: Yup.string().required(),
-      open_on_weekends: Yup.boolean().required(),
-      images: Yup.array(
-        Yup.object().shape({
-          path: Yup.string().required(),
-        })
-      ),
-    });
-
-    await schema.validate(data, {
+    await schemaOrphanage.validate(data, {
       abortEarly: false,
     });
 
@@ -52,9 +40,9 @@ class OrphanagesService {
 
     return {
       status: httpStatus.CREATED,
-      result: orphanage
-    }
-  }
+      result: orphanage,
+    };
+  };
 }
 
-export default new OrphanagesService()
+export default new OrphanagesService();
