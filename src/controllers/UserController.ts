b5a97c1eb95c702c8f3fb8384e9c26ce21437c6d
AlisonMarques <alison-marques@outlook.com.br>
2020-11-05 22:35:55 -0300

    verificando se usuário já existe pelo email

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
 
 import { UserService } from '../services';
+
 import schemaUser from '../schemas/schemaUser';
+import User from '../models/User';
 
 class UserController {
   async create(request: Request, response: Response) {
@@ -9,6 +12,16 @@ class UserController {
 
     if (!(await schemaUser.isValid(request.body))) {
       return response.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const userRepository = getRepository(User);
+
+    const userExists = await userRepository.findOne({
+      where: { email: request.body.email },
+    });
+
+    if (userExists) {
+      return response.status(401).json({ error: 'User already exists' });
     }
 
     const data = {
